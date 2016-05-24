cb953215b2fbd7e1f6e69c698aaca410e80aee76
William Meng <wlmeng11@gmail.com>
2016-05-24 09:29:02 -0700

    Custom Django command for populating the database with classifications

---
+++ getInvolvedPAWebsite/servicediscovery/management/commands/populate_database.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# This script populates the database with a preset list of classifications
+# and services from a CSV file (downloaded from the Google spreadsheet).
+#
+# See https://docs.djangoproject.com/en/1.9/howto/custom-management-commands/#module-django.core.management
+# for help on writing Django custom commands.
+#
+# Usage:
+# python manage.py populate_database
+
+from django.core.management.base import BaseCommand, CommandError
+from servicediscovery.models import *
+
+service_classifications = ['Education',
+                           'Sports',
+                           'Seniors/Disabled',
+                           'Animals/Environment',
+                           'Civic Engagement',
+                           'Home Construction',
+                           'Health Care/Safety',
+                           'Humanitarianism',
+                           'Food/Shelter']
+
+class Command(BaseCommand):
+    help = 'Populate the database with a preset list of classifications'
+
+    def handle(self, *args, **options):
+        # now do the things that you want with your models here
+        for service_classification in service_classifications:
+            # TODO: avoid adding duplicates!
+            s = Service_Classification(class_name=service_classification)
+            s.save()
+
+
